58ff64953fbe192e62ebaec450a84481428001d5
Akhtamohammad-naji <53796500+Naji-moradi@users.noreply.github.com>
2023-05-01 15:17:03 +0530

    as a test
    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to the wikiAryia project</div>;
+  return <div className="App">Welcome to the wikiAryia project test</div>;
 }
 
 export default App;
