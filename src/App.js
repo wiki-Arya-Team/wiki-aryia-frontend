7f1bc73114c36a4f67d28977e7b7dd5d047a441e
Akhtamohammad-naji <53796500+Naji-moradi@users.noreply.github.com>
2023-05-02 22:55:19 +0530

    Routing
    Base of routing is done with examples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 function App() {
-  return <div className="App">Welcome to the wikiAryia project test</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/register" element={<SignUpPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
