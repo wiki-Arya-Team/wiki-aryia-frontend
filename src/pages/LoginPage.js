550f2c6ce4f493959363dfb0161dedd6672c1020
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-05-03 13:13:25 +0430

    Complete log-in and sign-up pages

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { PersonFill, KeyFill, ArrowRight } from "react-bootstrap-icons";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="container">
+      <div className="login container__login">
+        <form action="#">
+          <div className="login__field">
+            <PersonFill />
+            <input
+              type="email"
+              placeholder="User name/ Email"
+              required
+              className="login__input"
+            />
+          </div>
+
+          <div className="login__field">
+            <KeyFill />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              className="login__input"
+            />
+          </div>
+
+          <button type="submit" className="login__submit">
+            <span>LOG IN NOW</span>
+            <ArrowRight />
+          </button>
+        </form>
+      </div>
+
+      <div className="background container__background">
+        <span className="background__shape background__shape1"></span>
+        <span className="background__shape background__shape2"></span>
+        <span className="background__shape background__shape3"></span>
+        <span className="background__shape background__shape4"></span>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
