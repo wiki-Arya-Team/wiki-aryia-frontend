18fd3b08d12888509e1aa7909e0aa53dc06d1d7d
Arif Fakoizada <47653016+Arif1997@users.noreply.github.com>
2023-05-28 19:00:18 +0530

    the navbar structure and the routings have been changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
+import '../css/navbar.css';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <header className="header">Navbar</header>;
+  return <header className="header">
+    <ul style={{ display: 'flex' }}>
+      <li>
+        <Link to='register'>Register</Link>
+      </li>
+      <li>
+        <Link to='login'>Login</Link>
+      </li>
+      <li>
+        <Link to='demo'>Demo</Link>
+      </li>
+    </ul>
+
+  </header>;
 };
 
 export default Navbar;
